d862ecc2612ed89e546196df0de2d0392e0edbb1
Scott <scott@newcompute.com>
2024-10-03 21:39:57 +0100

    Add PIA

---
+++ apps/desktop/pia/install.sh
@@ -0,0 +1,6 @@
+cd /tmp
+wget https://installers.privateinternetaccess.com/download/pia-linux-3.6.1-08339.run
+sudo chmod +x pia-linux-3.6.1-08339.run
+./pia-linux-3.6.1-08339.run
+rm pia-linux-3.6.1-08339.run
+cd -
